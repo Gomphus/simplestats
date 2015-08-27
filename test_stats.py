a972c97bcb893c6ac98adf2346a93c5a8db78ef9
Edgar Spalding <spalding@wisc.edu>
2015-08-27 13:41:20 -0500

    adding nose test

--- test_stats.py
+++ test_stats.py
@@ -1,18 +1,19 @@
 
 from stats import mean
+from nose.tools import assert_equal
 
 def mean(vals):
 	total = sum(vals)
 	length = len(vals)
 	return total/length
 def test_mean():
-	assert(mean([2,4]) == 3)
-test_mean()
+	assert_equal(mean([2,4]), 3)
+#test_mean()
 
 def test_float_mean():
-	assert(mean([1,2]) == 1.5)
-test_float_mean()
+	assert_equal(mean([1,2]), 1.5)
+#test_float_mean()
 
 def test_newmean():
-	assert(test_newmean(-2,2) == 0)
-test_newmean()python 
+	assert_equal(test_newmean(-2,2), 0)
+#test_newmean()
